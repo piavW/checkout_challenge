3dfaab8709a159eb723404df3096f04a10f11e81
piavw <piavonwachenfelt@hotmail.com>
2019-09-05 16:54:13 +0200

    Adds tests for the other commodities.

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,4 +4,13 @@ describe Commodity do
     it 'has the type as tie have the number 001' do
         expect(subject.tie).to eq 001
     end
+
+    it 'has the type of sweater with number 002' do
+        expect(subject.sweater).to eq 002
+    end
+
+    it 'has the type of skirt with number 003' do
+        expect(subject.skirt).to eq 003
+    end 
+
 end
